c8624a4cb91b97dd7c60fd750bc65f84ee4af0b3
vinit-rao <virtualvinityt@gmail.com>
2026-02-20 16:39:45 -0500

    images in post

--- gamehotspot/models.py
+++ gamehotspot/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     body = models.TextField()
     slug = models.SlugField(max_length=100, unique=True)
     date = models.DateTimeField(auto_now_add=True)
+    banner = models.ImageField(default='fm5.jpg', blank=True)
 
     def __str__(self):
         return self.title
--- term_project/urls.py
+++ term_project/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from gamehotspot import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('tutorial/', views.tutorial, name='tutorial'),
     path('about/', views.about, name='about'),
     path('posts/', include('gamehotspot.urls'))
-
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
