8ec3b1037b9442b54e96d13b0242405e74e5b4b7
vinit-rao <virtualvinityt@gmail.com>
2026-02-21 13:10:53 -0500

    login/register, create posts save, layout refine

--- queueup/views.py
+++ queueup/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import Post
+from django.contrib.auth.decorators import login_required
+from . import forms
 
 def home(request):
     return render(request, 'home.html')
@@ -18,3 +20,16 @@ def posts_list(request):
 def post_page(request, slug):
     post= Post.objects.get(slug=slug)
     return render(request, 'posts/post_page.html', {'post': post})
+
+@login_required(login_url='/users/login')
+def post_new(request):
+    if request.method == 'POST':
+        form = forms.CreatePost(request.POST, request.FILES)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.author = request.user
+            new_post.save()
+            return redirect('posts:list')
+    else:
+        form = forms.CreatePost()
+    return render(request, 'posts/post_new.html', {'form': form})
--- term_project/urls.py
+++ term_project/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('posts/', include('queueup.urls')),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
