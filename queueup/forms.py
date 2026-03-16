ffa8767009dc2c43b7cebe3227e9e0f1f49787bc
vinit-rao <virtualvinityt@gmail.com>
2026-03-16 04:07:55 -0400

    Display game banner, autoslug, ux polish, slight css changes, game igdb registry

--- queueup/forms.py
+++ queueup/forms.py
@@ -4,4 +4,4 @@ from . import models
 class CreatePost(forms.ModelForm):
     class Meta:
         model = models.Post
-        fields = ['title', 'body', 'slug', 'banner']
+        fields = ['title', 'body', 'game_name']
--- queueup/views.py
+++ queueup/views.py
@@ -1,8 +1,14 @@
+import uuid
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
-from . models import Post
+from django.utils.text import slugify
+from .igdb import fetch_game_data, get_game_suggestions
+
+from .forms import CreatePost
+from .models import Post
 from django.contrib.auth.decorators import login_required
 from . import forms
+from django.db.models import Q
 
 #Diako Search Feature
 from django.db.models import Q
@@ -36,17 +42,29 @@ def post_page(request, slug):
     post= Post.objects.get(slug=slug)
     return render(request, 'posts/post_page.html', {'post': post})
 
-@login_required(login_url='/users/login')
+@login_required(login_url='/users/login/')
 def post_new(request):
     if request.method == 'POST':
         form = forms.CreatePost(request.POST, request.FILES)
         if form.is_valid():
-            new_post = form.save(commit=False)
-            new_post.author = request.user
-            new_post.save()
+            post = form.save(commit=False)
+            post.author = request.user
+
+            unique_id = uuid.uuid4().hex[:6]
+            post.slug = f"{slugify(post.title)}-{unique_id}"
+
+            game_search_query = form.cleaned_data.get('game_name')
+            if game_search_query:
+                game_data = fetch_game_data(game_search_query)
+                if game_data:
+                    post.game_name = game_data['name']
+                    post.cover_url = game_data['cover_url']
+                    post.banner_url = game_data['banner_url']
+
+            post.save()
             return redirect('posts:list')
     else:
-        form = forms.CreatePost()
+        form = CreatePost()
     return render(request, 'posts/post_new.html', {'form': form})
 
 
@@ -59,3 +77,11 @@ def lobby_view(request, lobby_name):
         'lobby_name': lobby_name,
         'messages': messages
     })
+
+
+def api_search_games(request):
+    query = request.GET.get('q', '')
+    if len(query) < 2:
+        return JsonResponse({'results': []})
+    suggestions = get_game_suggestions(query)
+    return JsonResponse({'results': suggestions})
