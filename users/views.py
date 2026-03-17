ee76124c03f1df171db63da682f3d759a83e853e
vinit-rao <virtualvinityt@gmail.com>
2026-03-17 00:48:30 -0400

    User DOB now registered on users, existing users dont need DOB to be functional

--- users/views.py
+++ users/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
-# Create your views here.
+from .forms import CustomRegisterForm
+
 
 def register_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomRegisterForm(request.POST)
         if form.is_valid():
             login(request, form.save())
             return redirect("posts:list")
     else:
-        form = UserCreationForm()
+        form = CustomRegisterForm()
+
     return render(request, "users/register.html", {"form": form})
 
 def login_view(request):
